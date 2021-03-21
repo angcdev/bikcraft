088434922cba44cf634d48aee0cf7841f9a089e1
Angela Cavalcanti <angelacavalcanti94@gmail.com>
2021-03-21 15:52:10 -0300

    terminei as animações

--- código/js/main.js
+++ código/js/main.js
@@ -11,27 +11,27 @@ $(".rslides").responsiveSlides({
     pager: true,           // Boolean: Show pager, true or false
   });
 
-  Visibility.onVisible(function(){
-    setTimeout(function () {
-      $(".intro h1").addClass("animated fadeInDown");
-    }, 400);
-    setTimeout(function () {
-      $(".intro blockquote").addClass("animated fadeInDown");
-    }, 800);
-    setTimeout(function () {
-      $(".intro .btn").addClass("animated fadeInDown");
-    }, 1200);
-    setTimeout(function () {
-      $(".animar").addClass("animated fadeInDown");
-    }, 1600);
+// Visibility.onVisible(function(){
+//   setTimeout(function () {
+//     $(".intro h1").addClass("animate__animated animate__fadeInDown");
+//   }, 400);
+//     // setTimeout(function () {
+//     //   $(".intro blockquote").addClass("animated fadeInDown");
+//     // }, 800);
+//     // setTimeout(function () {
+//     //   $(".intro .btn").addClass("animated fadeInDown");
+//     // }, 1200);
+//     // setTimeout(function () {
+//     //   $(".animar").addClass("animated fadeInDown");
+//     // }, 1600);
   
-    setTimeout(function () {
-      $(".intro-interna h1").addClass("animated fadeInDown");
-    }, 400);
-    setTimeout(function () {
-      $(".intro-interna p").addClass("animated fadeInDown");
-    }, 800);
-    setTimeout(function () {
-      $(".animar-interno").addClass("animated fadeInDown");
-    }, 1200);
-  });
+//     // setTimeout(function () {
+//     //   $(".intro-interna h1").addClass("animated fadeInDown");
+//     // }, 400);
+//     // setTimeout(function () {
+//     //   $(".intro-interna p").addClass("animated fadeInDown");
+//     // }, 800);
+//     // setTimeout(function () {
+//     //   $(".animar-interno").addClass("animated fadeInDown");
+//     // }, 1200);
+// });
